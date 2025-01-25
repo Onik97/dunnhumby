b03d2fc8a46404d59f61a1ce9f2b13ed35eaf752
Onik <O_Noor@live.co.uk>
2025-01-25 01:42:08 +0000

    Routing clean up

--- Dunnhumby.Web/src/Layout.tsx
+++ Dunnhumby.Web/src/Layout.tsx
@@ -1,6 +1,11 @@
 import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
-import { Routing } from "./Main";
+import Container from "@/components/Container";
+import { RoutingProps } from "@/Routing";
+
+interface Routing {
+  routing: RoutingProps[];
+}
 
 export default function Layout({ routing }: Routing) {
   return (
---
+++ Dunnhumby.Web/src/Routing.tsx
@@ -0,0 +1,52 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "@/pages/ErrorPage";
+import Layout from "./Layout";
+import HomePage from "@/pages/HomePage";
+import CreateProductPage from "./pages/Product/CreateProductPage";
+import StockPerCategoryPage from "./pages/Product/StockPerCategoryPage";
+
+export interface RoutingProps {
+  label?: string;
+  navRouting?: boolean;
+  profileRouting?: boolean;
+  route: RouteObject;
+}
+
+const routesWithLabels: RoutingProps[] = [
+  {
+    label: "Home",
+    route: {
+      path: "/",
+      element: <HomePage />,
+    },
+  },
+  {
+    label: "Create Product",
+    navRouting: true,
+    route: {
+      path: "/product/create",
+      element: <CreateProductPage />,
+    },
+  },
+  {
+    label: "Stock Per Category",
+    navRouting: true,
+    route: {
+      path: "/product/stockcategory",
+      element: <StockPerCategoryPage />,
+    },
+  },
+  {
+    route: {
+      path: "*",
+      element: <ErrorPage />,
+    },
+  },
+];
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout routing={routesWithLabels} />,
+    children: routesWithLabels.map((route) => route.route),
+  },
+]);
